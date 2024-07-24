91724093f13530bab05064da795c7eb2ad0407b2
Muhammad-Bilal079 <mb4462087@gmail.com>
2024-07-25 02:26:37 +0500

    registration with jwt

--- Registration_JWT/src/controllers/registrationController.js
+++ Registration_JWT/src/controllers/registrationController.js
@@ -1,16 +1,43 @@
 import User from "../models/user.js";
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
-let registrationController = (req,res)=>{
+let registrationController = async (req, res) => {
+
+    // Find a user by email
+    const existingUser = await User.findOne({ email: req.body.email });
+
+    if (existingUser) {
+        // If user exists, respond with the user data
+        return res.status(200).json({
+            msg: "User already exixst"
+        });
+    }
+
+    // bcrypt password ya hash password 
+    let saltRounds = 10
+    let encryptedpassword = bcrypt.hashSync(req.body.password, saltRounds);
+    req.body.password = encryptedpassword
 
     // db code here 
-const user = new User(req.body);
-user.save()
-.then(
-    res.status(200).json({
-    msg:"user saved"
-}))
+    const user = new User(req.body);
+    user.save()
+        .then(d => {
+            // JWT Token 
+            // res send karnay say pehlay jwt token generate karna hota hai phir res send karna hota hai
+            var token = jwt.sign(req.body, process.env.JWT_TOKEN)
+            console.log("jwt token ==>",token);
 
-    
+            res.status(200).json({
+                msg: "user saved",
+                userToken:token
+            });
+        }).catch(e => {
+            res.status(400).json({
+                msg: "error "
+            })
+        })
+
 }
 
 export default registrationController
--- Registration_JWT/src/models/user.js
+++ Registration_JWT/src/models/user.js
@@ -1,11 +1,24 @@
 import mongoose from "../config/db.js";
 
-let userSchema =  { 
-    name: String,
-    email:String,
-    username:String
+//schema define krnay kay do tarikay hain pehla kay simple js object or dusra yaeh class or methods 
+//wala yeah ziada efficent hai is liay used kia hai
 
-}
+let userSchema = new mongoose.Schema(
+    { 
+        name: String,
+        email:{
+            type:String,
+            required:true
+        },
+        username:String,
+        password:String
+    
+    }
+    ,{
+        timestamps:true
+    }
+)
+ 
 
 const User = mongoose .model('User',userSchema);
 
