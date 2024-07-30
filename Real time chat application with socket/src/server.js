b531040b2adb81efd2ff882fd32e38103155cfc4
Muhammad-Bilal079 <mb4462087@gmail.com>
2024-07-30 16:34:41 +0500

    real time chat application

--- Real time chat application with socket/src/server.js
+++ Real time chat application with socket/src/server.js
@@ -1,8 +1,31 @@
 import express from 'express';
+const app = express();
+
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-const app = express();
+import http from 'http'
+import { Server } from "socket.io";
+
+const server = http.createServer(app)
+const io = new Server(server);
+
+// that can help to find client is conneted or not 
+io.on("connection", (socket) => {
+    console.log('client conencted',socket.id);
+    socket.on("disconnect",()=>{
+        console.log('client disconnected',socket.id);
+    })
+    // listen 
+socket.on('clientEvent',(payload)=>{
+console.log('client payload',payload);
+// speak 
+io.emit('serverEvent',payload)
+})
+  });
+
+ 
+//yeah do lines file kay path kay liay hain
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -10,6 +33,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000, () => {
+server.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
