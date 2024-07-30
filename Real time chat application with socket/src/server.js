cd12bff5eef374fbf8c555ab0ea3b15bbbc0866e
Muhammad-Bilal079 <mb4462087@gmail.com>
2024-07-30 05:06:46 +0500

    chat application

---
+++ Real time chat application with socket/src/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
