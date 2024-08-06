fa5fa528121110e2cbce8bef845cc41984971788
Muhammad-Bilal079 <mb4462087@gmail.com>
2024-08-07 01:07:28 +0500

    jwt

--- Registration_JWT/src/config/db.js
+++ Registration_JWT/src/config/db.js
@@ -3,8 +3,10 @@ import mongoose from 'mongoose';
 dotenv.config();
 
 // Database connection
-mongoose.connect(`mongodb+srv://${process.env.MONGOUSERNAME}:${process.env.MONGOPASSWORD}@cluster0.hsjhrzt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
-.then(d=>console.log('Db connected'))
-.catch(e=>console.log('DB error',e))
+// mongoose.connect(`mongodb+srv://${process.env.MONGOUSERNAME}:${process.env.MONGOPASSWORD}@cluster0.hsjhrzt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+// .then(d=>console.log('Db connected'))
+// .catch(e=>console.log('DB error',e))
+
+// yeah wala database ka url use nahi karna hai qk yeah fyp kay project main used kar lia hai  
 
 export default mongoose
